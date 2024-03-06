1b9535409c06ef3da5aa337532ac03d21d94d6e0
mooud <mooudgamer@gmail.com>
2024-03-06 16:58:47 +0330

    Setup tailwindcss complated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/renderer/index.html","./**/*.{tsx,jsx,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
